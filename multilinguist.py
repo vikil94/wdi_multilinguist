47fb73cc50d28f78154a74823403300e8d390b88
Vikil Naik <vikilnaik37@gmail.com>
2019-03-06 10:40:32 -0500

    Finished excercise

--- multilinguist.py
+++ multilinguist.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 class Multilinguist:
-  """This class represents a world traveller who knows 
+  """This class represents a world traveller who knows
   what languages are spoken in each country around the world
   and can cobble together a sentence in most of them
   (but not very well)
@@ -15,7 +15,7 @@ class Multilinguist:
 
   def __init__(self):
     """Initializes the multilinguist's current_lang to 'en'
-    
+
     Returns
     -------
     Multilinguist
@@ -34,7 +34,7 @@ class Multilinguist:
 
     Returns
     -------
-    bool 
+    bool
         2 letter iso639_1 language code.
     """
     params = {'fullText': 'true'}
@@ -78,3 +78,37 @@ class Multilinguist:
     json_response = json.loads(response.text)
     return json_response['translationText']
 
+
+class MathGenius(Multilinguist):
+
+
+    def report_total(self, list = []):
+        sum_total = sum(list)
+        return "{} {}".format(self.say_in_local_language('The total is'), sum_total)
+
+class QouteCollector(Multilinguist):
+
+
+    quotes = [
+    "You have to feel a defeat. You cannot say ‘I don’t care, it’s not important’. If I was allowed to say sh*t I would say sh*t but I’m not allowed! It was important and we lost, so that feels not too good. You always have to strike back. We can say all of these things, but you know you can fall down and then you have to stand up. That’s the truth, but it’s completely normal – only silly idiots stay on the floor and wait for the next defeat. Of course we will strike back – 100%. We struck back today in the game."
+    ]
+
+    def collector(self, quote):
+        self.qoutes.append(quote)
+
+    def generator(self):
+        return self.say_in_local_language(random.choice(self.quotes))
+
+
+# me = MathGenius()
+# print(me.report_total([23,45,676,34,5778,4,23,5465])) # The total is 12048
+# me.travel_to("India")
+# print(me.report_total([6,3,6,68,455,4,467,57,4,534])) # है को कुल 1604
+# me.travel_to("Italy")
+# print(me.report_total([324,245,6,343647,686545])) # È Il totale 1030767
+
+
+traveller_1 = Multilinguist()
+traveller_1.language_in('Zimbabwe')
+traveller_1.travel_to('France')
+print(traveller_1.say_in_local_language("Lets play some soccer"))
